d1389da76e038941287a48ecb4ad6743e6fa6682
sharon-ku <sharon-ku@users.noreply.github.com>
2021-07-22 09:39:32 -0400

    Game: set up canvas; centered body elements; set up background color code
    I chose to make my game on a 1280x720 canvas. Not completely set on that yet, since I want the game to also be accessible on mobile. But I like the idea of it being landscape and higher resolution so that the player can really absorb the experience of the memory.
    
    One thing I can probably do for mobile is prompt the player to rotate their phone when it's in portrait mode so that I can code less and maintain the landscape mode.

--- js/script.js
+++ js/script.js
@@ -6,12 +6,62 @@ This canvas is used in the background of the game.
 Lengthier description will be provided once I actually have stuff in here!
 **************************************************/
 
+"use strict";
+
+// State
+// All possibilities: intro, game, end
+let state = `game`;
+
+// Background color for different states
+let bgFill = {
+  current: {
+    r: undefined,
+    g: undefined,
+    b: undefined,
+  },
+  intro: {
+    r: 226,
+    g: 248,
+    b: 249,
+  },
+  game: {
+    r: 226,
+    g: 248,
+    b: 249,
+  },
+  end: {
+    r: 244,
+    g: 208,
+    b: 220,
+  },
+};
+
 // setup()
 //
 // Description of setup() goes here.
-function setup() {}
+function setup() {
+  createCanvas(1280, 720);
+}
 
 // draw()
 //
 // Description of draw() goes here.
-function draw() {}
+function draw() {
+  // Set backgroun color
+  background(bgFill.current.r, bgFill.current.g, bgFill.current.b);
+
+  // Set states and their background colors
+  if (state === `intro`) {
+    bgFill.current = bgFill.intro;
+    intro();
+  } else if (state === `game`) {
+    bgFill.current = bgFill.game;
+    game();
+  } else if (state === `end`) {
+    bgFill.current = bgFill.end;
+    end();
+  }
+}
+
+// Here's where the player chooses their memories
+function game() {}
